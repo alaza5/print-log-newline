74cc31880d3b770f1610cc0cf6c7b77949cbdc8b
alaza5 <wojciech3313@gmail.com>
2024-04-27 15:41:39 +0200

    Added somewhat working code

---
+++ Makefile
@@ -0,0 +1,3 @@
+install:
+	vsce package --allow-missing-repository
+	code --install-extension print-logger-shortcut-1.0.0.vsix
--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,80 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+/* eslint-disable @typescript-eslint/naming-convention */
+import * as vscode from "vscode";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+const defaultLoggerMap: Record<string, string> = {
+  fsharp: 'printfn $"${label} {value}"',
+  go: 'fmt.Println("${label}", ${label})',
+  java: 'System.out.println("${label}", ${value});',
+  javascript: 'console.log("${label}", ${value});',
+  javascriptreact: 'console.log("${label}", ${value});',
+  rust: 'println!("{} ${label}", ${value});',
+  typescript: 'console.log("${label}", ${value});',
+  typescriptreact: 'console.log("${label}", ${value});',
+};
+
 export function activate(context: vscode.ExtensionContext) {
+  const printCommand = vscode.commands.registerCommand(
+    "printLoggerShortcut.insertPrint",
+    async () => {
+      const editor = vscode.window.activeTextEditor;
+      if (!editor) {
+        vscode.window.showErrorMessage("No active editor found");
+        return;
+      }
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "print-logger-shortcut" is now active!');
+      const selectedCode = editor.document.getText(editor.selection);
+      if (!selectedCode) {
+        return;
+      }
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('print-logger-shortcut.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from print_logger_shortcut!');
-	});
+      const languageId = vscode.window.activeTextEditor?.document.languageId;
+      if (!languageId) {
+        vscode.window.showErrorMessage("Unknow language: " + languageId);
+        return;
+      }
 
-	context.subscriptions.push(disposable);
+      const config: vscode.WorkspaceConfiguration =
+        vscode.workspace.getConfiguration("printLoggerShortcut");
+
+      const logPrefix = config?.logPrefix ?? "";
+      const loggerMap = config?.loggerMap;
+
+      const snippetString =
+        loggerMap?.[languageId] ?? defaultLoggerMap[languageId];
+      if (!snippetString) {
+        vscode.window.showErrorMessage("Unsupported language: " + languageId);
+        return;
+      }
+
+      const newLineRow = editor.selection.active.line + 1;
+
+      await vscode.commands.executeCommand("editor.action.insertLineAfter");
+
+      await editor
+        .edit((editBuilder: vscode.TextEditorEdit) => {
+          const insertedLine = editor.document.lineAt(newLineRow);
+
+          const contentPos = new vscode.Position(
+            newLineRow,
+            insertedLine.firstNonWhitespaceCharacterIndex,
+          );
+
+          const content = snippetString
+            .replace("${label}", `${logPrefix}${selectedCode}`)
+            .replace("${value}", selectedCode);
+
+          editBuilder.insert(contentPos, content);
+        })
+        .then(() => {
+          const vimExtension = vscode.extensions.getExtension("vscodevim.vim");
+          if (vimExtension) {
+            vscode.commands.executeCommand("extension.vim_escape");
+          }
+        });
+    },
+  );
+
+  context.subscriptions.push(printCommand);
 }
 
 // This method is called when your extension is deactivated
